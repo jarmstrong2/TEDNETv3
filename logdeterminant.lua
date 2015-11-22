3469f6b9d4d88472740d71a45f9e790ae964ecff
John Armstrong <j2armstrong@gmail.com>
2015-11-22 18:36:49 -0500

    Update logdeterminant.lua

--- logdeterminant.lua
+++ logdeterminant.lua
@@ -3,24 +3,24 @@ require 'nn'
 local LogDeterminant, parent = torch.class('nn.LogDeterminant', 'nn.Module')
 
 function LogDeterminant:updateOutput(input)
-    batchSize = input:size()[1]
+    local batchSize = input:size()[1]
     self.output = torch.CudaTensor(batchSize, 1)
     for i = 1, batchSize do
-        inputSize = ((input[i]):size())[1]
+        local inputSize = ((input[i]):size())[1]
         --eps = torch.eye(inputSize):float() * 1e-2
-        eig_vals = torch.eig(input[i], 'N')
+        local eig_vals = torch.eig(input[i], 'N')
         self.output[i] = (torch.log(eig_vals:select(2, 1))):sum()
     end
     return self.output
 end
 
 function LogDeterminant:updateGradInput(input, gradOutput)
-    batchSize = input:size()[1]
+    local batchSize = input:size()[1]
     self.gradInput =  torch.CudaTensor(input:size())
     for i = 1, batchSize do  
-        inputSize = ((input[i]):size())[1]
+        local inputSize = ((input[i]):size())[1]
         --eps = torch.eye(inputSize):float() * 1e-2
-        invInput = torch.inverse(input[i]) 
+        local invInput = torch.inverse(input[i]) 
         self.gradInput[i] = invInput:t() * gradOutput[i]:squeeze()
     end
     return self.gradInput
