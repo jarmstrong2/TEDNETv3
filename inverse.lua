7eea60c02ae0a92d6ab22dae2bb4a63473fbbb30
jarmstong2 <j2armstrong@gmail.com>
2015-11-22 00:44:29 -0500

    umm

--- inverse.lua
+++ inverse.lua
@@ -5,11 +5,11 @@ local Inverse, parent = torch.class('nn.Inverse', 'nn.Module')
 function Inverse:updateOutput(input)
     batchSize = input:size()[1]
     dim = input:size()[2]
-    self.output = torch.CudaTensor(batchSize, dim, dim)
+    self.output = torch.zeros(batchSize, dim, dim)
     for i = 1, batchSize do
         inputSize = ((input[i]):size())[1]
-        eps = torch.eye(inputSize):float() * 1e-1
-        self.output[i] = (torch.inverse(input:float()[i] + eps)):cuda()
+        eps = torch.eye(inputSize) * 1e-2
+        self.output[i] = torch.inverse(input[i] + eps)
     end
     return self.output
 end
@@ -17,12 +17,12 @@ end
 function Inverse:updateGradInput(input, gradOutput)
     batchSize = input:size()[1]
     dim = input:size()[2]
-    self.gradInput =  torch.CudaTensor(batchSize, dim, dim)
+    self.gradInput =  torch.zeros(batchSize, dim, dim)
     for i = 1, batchSize do
         inputSize = ((input[i]):size())[1]
-        eps = torch.eye(inputSize):float() * 1e-1  
-        self.gradInput[i] = -((torch.inverse(input:float()[i] + eps)):cuda() * 
-            gradOutput[i]) * (torch.inverse(input:float()[i] + eps)):cuda()
+        eps = torch.eye(inputSize) * 1e-2  
+        self.gradInput[i] = (torch.inverse(input[i] + eps) * 
+            gradOutput[i]) * torch.inverse(input[i] + eps)
     end
     return self.gradInput
 end
--- train.lua
+++ train.lua
@@ -52,7 +52,7 @@ end
 
 function getValLoss()
     local valnumberOfPasses = opt.numPasses
-    local valcount = 550
+    local valcount = 1
     local valsampleSize = opt.batchSize
     local loss = 0
     local elems = 0
@@ -105,10 +105,8 @@ function getValLoss()
        
             pi, mu, u = unpack(output_y[t])
 
-            --loss = clones.criterion[t]:forward({pi:float(), mu:float(), u:float(),
-            --    cmaskMat[{{},{},{t}}]:float(), x_target:float()}):sum() + loss  
-            loss = clones.criterion[t]:forward({pi:cuda(), mu:cuda(), u:cuda(),
-                cmaskMat[{{},{},{t}}]:cuda(), x_target:cuda()}):sum() + loss       
+            loss = clones.criterion[t]:forward({pi:float(), mu:float(), u:float(),
+                cmaskMat[{{},{},{t}}]:float(), x_target:float()}):sum() + loss        
         end
 
         loss = loss/(valsampleSize * valnumberOfPasses)
@@ -192,13 +190,10 @@ function feval(x)
                  kappa_prev[t-1], w[t-1], lstm_c_h1[t-1], lstm_h_h1[t-1],
                  lstm_c_h2[t-1], lstm_h_h2[t-1], lstm_c_h3[t-1], lstm_h_h3[t-1], lstm_c_h4[t-1], lstm_h_h4[t-1]}))
        
-            pi, mu, u, _, _ = unpack(output_y[t])
+            pi, mu, u = unpack(output_y[t])
 
-            --input_crit[t] = {pi:float(), mu:float(), u:float(),
-            --cmaskMat[{{},{},{t}}]:float(), x_target:float()}
-
-            input_crit[t] = {pi:cuda(), mu:cuda(), u:cuda(),
-            cmaskMat[{{},{},{t}}]:cuda(), x_target:cuda()}
+            input_crit[t] = {pi:float(), mu:float(), u:float(),
+            cmaskMat[{{},{},{t}}]:float(), x_target:float()}
 
             loss = clones.criterion[t]:forward(input_crit[t]):sum() + loss 
         end
@@ -227,9 +222,8 @@ function feval(x)
             local x_target = inputMat[{{},{1,opt.inputSize},{t+1}}]:squeeze()
             
             -- criterion
-            --local grad_crit = clones.criterion[t]:backward(input_crit[t], torch.ones(sampleSize,1):float())            
-            local grad_crit = clones.criterion[t]:backward(input_crit[t], torch.ones(sampleSize,1):cuda())
-
+            local grad_crit = clones.criterion[t]:backward(input_crit[t], torch.ones(sampleSize,1):float())            
+            	
             d_pi, d_mu, d_u = unpack(grad_crit)
 
             -- model
