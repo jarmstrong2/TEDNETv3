d10b94730fcb8ce49b934c8cabf82fb78e6f3ed7
John Armstrong <j2armstrong@gmail.com>
2015-11-22 17:39:45 -0500

    Update logdeterminant.lua

--- logdeterminant.lua
+++ logdeterminant.lua
@@ -8,7 +8,7 @@ function LogDeterminant:updateOutput(input)
     for i = 1, batchSize do
         inputSize = ((input[i]):size())[1]
         --eps = torch.eye(inputSize):float() * 1e-2
-        eig_vals = torch.eig(input:[i], 'N')
+        eig_vals = torch.eig(input[i], 'N')
         self.output[i] = (torch.log(eig_vals:select(2, 1))):sum()
     end
     return self.output
